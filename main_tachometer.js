c0dac62d0923d6ae17a8eaa6f6bfb7bae4d792d4
my-cubeserv <113102922+my-cubeserv@users.noreply.github.com>
2023-11-26 16:38:35 +0100

    Update main_tachometer.js

--- main_tachometer.js
+++ main_tachometer.js
@@ -1,4 +1,3 @@
-//(function() {
 	
 let tmpl = document.createElement('template');	
 tmpl.innerHTML = `
@@ -83,17 +82,161 @@ constructor() {
 	this.style.display = "block";
 	this._shadowRoot = this.attachShadow({mode: "open"});
 	this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-	this.render();		
+
+	this._firstConnection = false;
+	this.wData = [];
+		this.properties = {
+	       color: "#000000",
+	       value: 5,
+		   fontsize: 20,
+		   bgcolor1: "#F12712",
+		   bgcolor2: "#22b326",
+		   bgcolor3: "#2681c5",
+		   bgcolor4: "#ECE859",
+		   bgcolor5: "#E035d3",
+		   bgcolor6: "#E08135"
+		};	
+	this.render();	
 	} 
+	get selection() {
+            const result = { ...this._selection, ...(this._selection || {}).measures_0 };
+            return Object.values(result).length > 0 ? result : undefined;
+        }
+	_submit(e) {
+			e.preventDefault();
+			this.dispatchEvent(new CustomEvent("propertiesChanged", {
+					detail: {
+						properties: {
+							color: this.color,
+							fontsize: this.fontsize,
+							bgcolor1: this.bgcolor1,
+							bgcolor2: this.bgcolor2,
+							bgcolor3: this.bgcolor3,
+							bgcolor4: this.bgcolor4,
+							bgcolor5: this.bgcolor5,
+							bgcolor6: this.bgcolor6
+						}
+					}
+			}));
+	}
+	//When the widget is added to the html DOM of the page
 	connectedCallback() {
    		this._firstConnection = true;
 		this.render();
-		//this.setstyles();
+		this.setstyles();
   	}
 
-		disconnectedCallback() {
+	//When the widget is removed from the html DOM of the page
+	disconnectedCallback() {
    this._connected = false;
   }
+  	 //When the custom widget is updated
+	onCustomWidgetBeforeUpdate(oChangedProperties) {
+		this._props = { ...this._props, ...oChangedProperties };
+	}
+	  //When the custom widget is updated
+	onCustomWidgetAfterUpdate(oChangedProperties) {
+		this._needsRedraw = true;
+		this._selection = {};
+		if (oChangedProperties.myDataSource && !this._props.designMode) {
+			// trigger onResultChanged event
+			this.dispatchEvent(new Event("onResultChanged"));
+		}
+		
+		if ("color" in oChangedProperties) {
+				this.color = oChangedProperties["color"];
+			} 
+		if ("fontsize" in oChangedProperties) {
+				this.fontsize = oChangedProperties["fontsize"];
+			} 
+		if ("bgcolor1" in oChangedProperties) {
+				this.bgcolor1 = oChangedProperties["bgcolor1"];
+			} 
+		if ("bgcolor2" in oChangedProperties) {
+				this.bgcolor2 = oChangedProperties["bgcolor2"];
+			} 
+		if ("bgcolor3" in oChangedProperties) {
+				this.bgcolor3 = oChangedProperties["bgcolor3"];
+			} 			
+		if ("bgcolor4" in oChangedProperties) {
+			this.bgcolor4 = oChangedProperties["bgcolor4"];
+		} 
+		if ("bgcolor5" in oChangedProperties) {
+			this.bgcolor5 = oChangedProperties["bgcolor5"];
+		} 
+		if ("bgcolor6" in oChangedProperties) {
+			this.bgcolor6 = oChangedProperties["bgcolor6"];
+		} 
+		this.redraw();
+    }
+	
+	 redraw() {
+		if (!this._shadowRoot) { return; }
+	   this._shadowRoot.textContent = "";
+	   // check the result state (could be "loading", "success" or "error")
+		const myDataSource = this._props.myDataSource;
+		if (myDataSource)
+		{
+			switch (myDataSource.state) {
+				case "loading": {
+					this._shadowRoot.innerHTML = "Loading...";
+					return;
+				}
+				case "error": {
+					if (myDataSource.messages.length) {
+						this._shadowRoot.innerHTML = "<h1>Could not render chart</h1>" + (myDataSource.messages || []).map(m => `<b>${m.level}</b>: ${m.message}`).join("br");
+					} else {
+						// in case of no data an appropriate message will be show
+						this._shadowRoot.innerHTML = "<h1>No data</h1>";
+					}
+					return;
+				} case "success": {
+					this.redrawChart();
+					return;
+				}
+			}
+		}
+	}
+	redrawChart() {            
+		// indicate result state: "error" and an appropriate message
+		const myDataSource = this._props.myDataSource;
+		/*if (!myDataSource.data.length
+			|| Object.keys(myDataSource.metadata.dimensions).length === 0
+			|| Object.keys(myDataSource.metadata.mainStructureMembers).length === 0) {
+			this._shadowRoot.innerHTML = "<h1>No data</h1>";
+			return;
+		}*/
+		this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+		
+		const data = myDataSource.data;
+		
+		if(data)
+		{
+			this.wData = this.parseData(JSON.parse(JSON.stringify(data)));
+							
+			if (this.wData.length === 0) {
+				this._shadowRoot.innerHTML = "<h1>Select \"Include Parent Elements\" in Hierarchy Menu.</h1>";
+				return;
+			}
+			this.render();
+		}
+	}
+
+	//Collect data array
+	parseData(ndata){	
+		
+		let alldata = [];
+		
+		ndata.forEach(function (item) {
+			if(item.dimensions_0 && item.measures_0)
+			{
+				alldata.push([item.measures_0.raw, item.dimensions_0.id ]);
+			}					
+		 });
+		 alldata.sort(function(a, b){return b[0] - a[0]});			
+		return 	alldata;
+	}
+	
 	//When the custom widget resized
 onCustomWidgetResize() {
 	this.width = this._shadowRoot.host.offsetWidth;
@@ -101,8 +244,8 @@ onCustomWidgetResize() {
         this._needsRedraw = true;
 	this.resize(this.width,this.height);
 	this.style.display = "block";
-	this.render();	
-	location.reload();
+	this.redraw();
+	//this.render();	
 	//this.setstyles( );
 	
 }
@@ -265,5 +408,3 @@ const label = ['10%', '20%', '30%', '40%'];
 	
    };
 	customElements.define("chart-tachometer",Tachometer); 
-
-//})();
