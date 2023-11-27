20170acc039dfdd4b2579a71a8179bd482d7d64e
my-cubeserv <113102922+my-cubeserv@users.noreply.github.com>
2023-11-27 14:16:57 +0000

    Update styling_tachometer.js

--- styling_tachometer.js
+++ styling_tachometer.js
@@ -132,14 +132,13 @@
             return false;
         }
 // function conver hex to rgb
- convercolor(hcolor, copasity) 
+ convercolor(hcolor) 
 {
     var aRgbHex = hcolor.match(/.{1,2}/g);
     var aRgb = [
         parseInt(aRgbHex[0], 16),
         parseInt(aRgbHex[1], 16),
-        parseInt(aRgbHex[2], 16),
-	this.opasity
+        parseInt(aRgbHex[2], 16)	
     ];
     return aRgb;
 }
