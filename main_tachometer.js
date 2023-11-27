a16c2a4a1411136265631f50252c3d6ad506927d
my-cubeserv <113102922+my-cubeserv@users.noreply.github.com>
2023-11-27 16:12:18 +0100

    Update main_tachometer.js

--- main_tachometer.js
+++ main_tachometer.js
@@ -362,6 +362,8 @@ render(ndata) {
 //**** end functions *****
 	
 function hex2rgba (hex , alpha ){
+   hex = hex + "";
+   alpha = alpha + "";
   let r = parseInt(hex.slice(1, 3), 16);
   let g = parseInt(hex.slice(3, 5), 16);
   let b = parseInt(hex.slice(5, 7), 16);
