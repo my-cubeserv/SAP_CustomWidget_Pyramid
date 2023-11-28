30a58d7a18ece2b8890061112eeed391d4087269
my-cubeserv <113102922+my-cubeserv@users.noreply.github.com>
2023-11-28 18:29:22 +0100

    Update main_tachometer.js

--- main_tachometer.js
+++ main_tachometer.js
@@ -16,7 +16,7 @@ tmpl.innerHTML = `
        	//	 width: 100%;
 		//height:100%;		
 		padding-left: 30px;	
-		padding-right: 10px;	
+		padding-right: 0px;	
 		position: relative;
 		//background: green;
 		background: transparent;
