ffb0ea2a7514ac666b3f3c0fbf8a2e12b8077ddf
my-cubeserv <113102922+my-cubeserv@users.noreply.github.com>
2023-11-28 09:55:54 +0000

    Update main_tachometer.js

--- main_tachometer.js
+++ main_tachometer.js
@@ -476,27 +476,10 @@ const label = ['10%', '20%', '30%', '40%'];
       datasets: [{
         label: 'Weekly Sales',
         data: [33, 33, 33 ],
-        backgroundColor: [ 
-          'rgba(255, 26, 104, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(0, 0, 0, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 26, 104, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(0, 0, 0, 1)'
-        ],
-								  
-        borderWidth: 1,
-		circumference: 180,
+        backgroundColor: backgroundcolors,
+        borderColor: bordercolor,
+        borderWidth: borderwith,					  
+      	circumference: 180,
 		rotation: 270,
 		cutot:'95%',
 		borderRadius: 10,
