c9df12c764c71d2d53a7cd26f769bffec9730435
my-cubeserv <113102922+my-cubeserv@users.noreply.github.com>
2023-12-07 17:30:59 +0000

    Update main_tachometer.js

--- main_tachometer.js
+++ main_tachometer.js
@@ -80,7 +80,7 @@ tmpl.innerHTML = `
 <div id = "DataLablesTop" class="labeltop" ></div>
 <div id = "DataLablesRight" class="labelright" ></div>	
 <div id = "DataInfo" class="datainfo"></div>
-<img id="Chartimg" src="" class="Chartimg"/>
+<img id="Chartimg" src="" class="chartimg"/>
 `;
 
 class Tachometer extends HTMLElement {	
@@ -321,10 +321,13 @@ onCustomWidgetResize() {
         this._needsRedraw = true;
 	this.resize(this.width,this.height);
 	this.redraw();
-	this._shadowRoot.getElementById("Chartimg").src = imgb;
-	//this._shadowRoot.getElementById("Tachometer").style.visibility = "hidden";
-	this._shadowRoot.getElementById("Chartimg").style.width = "300px";
-	this._shadowRoot.getElementById("Chartimg").style.height = "150px";
+	
+}
+setimage(){
+	this._shadowRoot.getElementById("Chartimg").src = imgb;	
+	this._shadowRoot.getElementById("Chartimg").style.width = this._shadowRoot.host.offsetWidth;
+	this._shadowRoot.getElementById("Chartimg").style.height = this._shadowRoot.host.offsetHeight;
+	this._shadowRoot.getElementById("Tachometer").style.visibility = "hidden";
 }
 resize(w,h)
 {
